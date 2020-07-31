9d62236eb89c22e80a60b469a41c467f5538ca61
Abdullah Zubair Tanwir <abdullahzubair075@gmail.com>
2020-07-31 14:00:48 +0500

    Updates

---
+++ precache-manifest.a8ebd7544bdb9c0b6b3c01fc612a1a52.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "231fa03200c9126058857e2935f7a6c4",
+    "url": "/react-hackernews/index.html"
+  },
+  {
+    "revision": "8a84d4554ca7396c7a13",
+    "url": "/react-hackernews/static/css/main.8ccd8985.chunk.css"
+  },
+  {
+    "revision": "5f9424f9236ccf4e6850",
+    "url": "/react-hackernews/static/js/2.37d07c28.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-hackernews/static/js/2.37d07c28.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a84d4554ca7396c7a13",
+    "url": "/react-hackernews/static/js/main.cd893d24.chunk.js"
+  },
+  {
+    "revision": "e24e07a78b223db17bd0",
+    "url": "/react-hackernews/static/js/runtime-main.95c8f90e.js"
+  }
+]);
