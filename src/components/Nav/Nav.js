0d764b54dbe9e69df4fde8938456ba037c50bfe0
Abdullah Zubair Tanwir <abdullahzubair075@gmail.com>
2020-07-31 14:09:24 +0500

    minor changes

---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Nav.css';
+
+const Nav = () => {
+
+    return (
+        <div className="nav">
+            <h1>React - Hacker News</h1>
+        </div>
+    )
+
+}
+
+export default Nav;
