60daa2cbbb26d21d448cb9cd6aadc0eaedb0f813
Abdullah Zubair Tanwir <abdullahzubair075@gmail.com>
2020-07-31 19:47:03 +0500

    Basic components added

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 
 import Nav from './components/Nav/Nav';
+import NewsList from './components/NewsList/NewsList';
 
 function App() {
   return (
     <div className="App">
       <Nav/>
+      <NewsList/>
     </div>
   );
 }
---
+++ src/components/NewsItem/NewsItem.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './NewsItem.css'
+
+const NewsItem = () => {
+
+    return (
+        <div className="item" >
+            <h1>Data</h1>
+        </div>
+    )
+
+}
+
+export default NewsItem;
